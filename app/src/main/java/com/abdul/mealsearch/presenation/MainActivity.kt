de902c34b9f0c0936e81b38e7551d14040783f5b
abdul <abduluptu@gmail.com>
2026-03-23 11:13:40 +0530

    Updated files

--- app/src/main/java/com/abdul/mealsearch/presenation/MainActivity.kt
+++ app/src/main/java/com/abdul/mealsearch/presenation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.abdul.mealsearch.presenation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.abdul.mealsearch.R
 
 import dagger.hilt.android.AndroidEntryPoint
